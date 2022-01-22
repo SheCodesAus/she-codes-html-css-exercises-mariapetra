a29095d99f726dd07f1fa651d149af9e91aa5f41
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-01-22 22:20:52 +0800

    js exercisex

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,5 +1,17 @@
 window.onload = function() {
     // start
+    const starterDiv = document.getElementById("starter-div");
+    const firstHeading = document.createElement("h2");
+    const secondHeading = document.createElement("h2");
+
+    firstHeading.innerText = "This is an H2 created with JS";
+    firstHeading.classList.add("js-header");
+    starterDiv.appendChild(firstHeading);
+
+    secondHeading.innerText = "This is the second heading";
+    secondHeading.classList.add("js-header");
+    secondHeading.id = "second-h2";
+    starterDiv.appendChild(secondHeading);
 
     // end
 }
@@ -8,13 +20,14 @@ window.onload = function() {
 function showName(){
     console.log('showName');
     // start
-
+    let name = document.getElementById("name").value;
+    document.getElementById("greeting-text").innerText = `Hi ${name}`
     // end
 }
 
 function changeColour(colour) {
     // start
-
+    document.body.style.backgroundColor = colour;
     // end
 }
 
@@ -24,6 +37,7 @@ function resetColour() {
 
 function toggleLightbulb() {
     // start
-
+    const lightbulb = document.getElementById("light");
+    lightbulb.classList.toggle("on")
     // end
 }
--- keyboard/script.js
+++ keyboard/script.js
@@ -1,23 +1,43 @@
 window.onload = function() {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     // start
-
+    let keyboard = document.getElementById("keyboard");
     // end
 
     alphabet.forEach(letter => {
         // start
+        let keyButton = document.createElement("input");
+        keyButton.type = "button";
+        keyButton.value = letter;
+        keyButton.addEventListener('click', event => {
+            console.log(event)
+            highlightLetter(event.target.id);
+        })
+        keyButton.classList.add("key");
+        keyButton.id = letter;
+        keyboard.appendChild(keyButton);
 
         // end
     });
 }
 
 // start
-
+document.addEventListener("keydown", event => {
+    highlightLetter(event.key);
+    keyboard.appendChild(keyButton);
+});
 // end
 
 
 function highlightLetter(letter){
     // start
-
+    console.log(letter)
+    let output = document.getElementById("output-text");
+    output.innerText = output.innerText + letter;
+    let key = document.getElementById(letter);
+    key.classList.toggle("selected");
+    setTimeout(function() {
+        key.classList.toggle("selected");
+    }, 150);
     // end
 }
